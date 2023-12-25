10a08ebfec2a1c8add01e7d609a3412d2d9b895a
Dhurim Kelmendi <dhkelmendi@gmail.com>
2023-12-25 21:19:50 +0100

    Split test functions to run faster

--- services/product_service_test.go
+++ services/product_service_test.go
@@ -11,7 +11,6 @@ import (
 
 func TestProductService(t *testing.T) {
 	t.Parallel()
-
 	service := services.GetProductServiceDefaultInstance()
 	ctx := context.Background()
 
@@ -24,167 +23,173 @@ func TestProductService(t *testing.T) {
 			t.Fatalf("failed to insert pack_sizes: %v", err)
 		}
 	})
-	t.Run("create product orders with 250, 500, 2000, 5000 pack_sizes", func(t *testing.T) {
-		createPackSizesPayload := payloads.ChangePackSizesPayload{
-			Sizes: []int{250, 500, 2000, 5000},
-		}
-		_, err := service.CreatePackSizes(ctx, &createPackSizesPayload)
+}
+func TestWithLargePackSizes(t *testing.T) {
+	t.Parallel()
+	service := services.GetProductServiceDefaultInstance()
+	ctx := context.Background()
+	createPackSizesPayload := payloads.ChangePackSizesPayload{
+		Sizes: []int{250, 500, 2000, 5000},
+	}
+	_, err := service.CreatePackSizes(ctx, &createPackSizesPayload)
+	if err != nil {
+		t.Fatalf("failed to insert pack_sizes: %v", err)
+	}
+	t.Run("create product order with amount 1", func(t *testing.T) {
+		expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 250})
+
+		productOrderToCreate := &payloads.CreateProductOrderPayload{}
+		productOrderToCreate.Amount = 1
+		createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
 		if err != nil {
-			t.Fatalf("failed to insert pack_sizes: %v", err)
+			t.Fatalf("error while creating product %+v", err)
 		}
-		t.Run("create product order with amount 1", func(t *testing.T) {
-			expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 250})
-
-			productOrderToCreate := &payloads.CreateProductOrderPayload{}
-			productOrderToCreate.Amount = 1
-			createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
-			if err != nil {
-				t.Fatalf("error while creating product %+v", err)
-			}
-			if !reflect.DeepEqual(expectedResult, createdProductOrder) {
-				t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
-			}
-		})
-		t.Run("create product order with amount 250", func(t *testing.T) {
-			expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 250})
-
-			productOrderToCreate := &payloads.CreateProductOrderPayload{}
-			productOrderToCreate.Amount = 250
-			createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
-			if err != nil {
-				t.Fatalf("error while creating product %+v", err)
-			}
-			if !reflect.DeepEqual(expectedResult, createdProductOrder) {
-				t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
-			}
-		})
-		t.Run("create product order with amount 251", func(t *testing.T) {
-
-			expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 500})
-
-			productOrderToCreate := &payloads.CreateProductOrderPayload{}
-			productOrderToCreate.Amount = 251
-			createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
-			if err != nil {
-				t.Fatalf("error while creating product %+v", err)
-			}
-			if !reflect.DeepEqual(expectedResult, createdProductOrder) {
-				t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
-			}
-		})
-		t.Run("create product order with amount 501", func(t *testing.T) {
-
-			expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 500})
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 250})
-
-			productOrderToCreate := &payloads.CreateProductOrderPayload{}
-			productOrderToCreate.Amount = 501
-			createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
-			if err != nil {
-				t.Fatalf("error while creating product %+v", err)
-			}
-			if !reflect.DeepEqual(expectedResult, createdProductOrder) {
-				t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
-			}
-		})
-		t.Run("create product order with amount 751", func(t *testing.T) {
-
-			expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 2, PackSize: 500})
-
-			productOrderToCreate := &payloads.CreateProductOrderPayload{}
-			productOrderToCreate.Amount = 751
-			createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
-			if err != nil {
-				t.Fatalf("error while creating product %+v", err)
-			}
-			if !reflect.DeepEqual(expectedResult, createdProductOrder) {
-				t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
-			}
-		})
-		t.Run("create product order with amount 1000", func(t *testing.T) {
-
-			expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 2, PackSize: 500})
-
-			productOrderToCreate := &payloads.CreateProductOrderPayload{}
-			productOrderToCreate.Amount = 1000
-			createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
-			if err != nil {
-				t.Fatalf("error while creating product %+v", err)
-			}
-			if !reflect.DeepEqual(expectedResult, createdProductOrder) {
-				t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
-			}
-		})
-		t.Run("create product order with amount 12001", func(t *testing.T) {
-
-			expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
-
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 250})
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 2000})
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 2, PackSize: 5000})
-
-			productOrderToCreate := &payloads.CreateProductOrderPayload{}
-			productOrderToCreate.Amount = 12001
-			createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
-			if err != nil {
-				t.Fatalf("error while creating product %+v", err)
-			}
-			if len(expectedResult) != len(createdProductOrder) {
-				t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
-			}
-			if !reflect.DeepEqual(expectedResult, createdProductOrder) {
-				t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
-			}
-		})
+		if !reflect.DeepEqual(expectedResult, createdProductOrder) {
+			t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
+		}
 	})
-	t.Run("create product orders with 23, 31, 53 pack_sizes", func(t *testing.T) {
-		createPackSizesPayload := payloads.ChangePackSizesPayload{
-			Sizes: []int{23, 31, 53},
-		}
-		_, err := service.CreatePackSizes(ctx, &createPackSizesPayload)
+	t.Run("create product order with amount 250", func(t *testing.T) {
+		expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 250})
+
+		productOrderToCreate := &payloads.CreateProductOrderPayload{}
+		productOrderToCreate.Amount = 250
+		createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
 		if err != nil {
-			t.Fatalf("failed to insert pack_sizes: %v", err)
+			t.Fatalf("error while creating product %+v", err)
 		}
-		t.Run("create product order with amount 263", func(t *testing.T) {
+		if !reflect.DeepEqual(expectedResult, createdProductOrder) {
+			t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
+		}
+	})
+	t.Run("create product order with amount 251", func(t *testing.T) {
 
-			expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+		expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 500})
 
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 2, PackSize: 23})
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 7, PackSize: 31})
+		productOrderToCreate := &payloads.CreateProductOrderPayload{}
+		productOrderToCreate.Amount = 251
+		createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
+		if err != nil {
+			t.Fatalf("error while creating product %+v", err)
+		}
+		if !reflect.DeepEqual(expectedResult, createdProductOrder) {
+			t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
+		}
+	})
+	t.Run("create product order with amount 501", func(t *testing.T) {
 
-			productOrderToCreate := &payloads.CreateProductOrderPayload{}
-			productOrderToCreate.Amount = 263
-			createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
-			if err != nil {
-				t.Fatalf("error while creating product %+v", err)
-			}
-			if !reflect.DeepEqual(expectedResult, createdProductOrder) {
-				t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
-			}
-		})
-		t.Run("create product order with amount 500k", func(t *testing.T) {
+		expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 500})
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 250})
 
-			expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+		productOrderToCreate := &payloads.CreateProductOrderPayload{}
+		productOrderToCreate.Amount = 501
+		createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
+		if err != nil {
+			t.Fatalf("error while creating product %+v", err)
+		}
+		if !reflect.DeepEqual(expectedResult, createdProductOrder) {
+			t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
+		}
+	})
+	t.Run("create product order with amount 751", func(t *testing.T) {
 
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 2, PackSize: 23})
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 7, PackSize: 31})
-			expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 9429, PackSize: 53})
+		expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 2, PackSize: 500})
 
-			productOrderToCreate := &payloads.CreateProductOrderPayload{}
-			productOrderToCreate.Amount = 500000
-			createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
-			if err != nil {
-				t.Fatalf("error while creating product %+v", err)
-			}
-			if !reflect.DeepEqual(expectedResult, createdProductOrder) {
-				t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
-			}
-		})
+		productOrderToCreate := &payloads.CreateProductOrderPayload{}
+		productOrderToCreate.Amount = 751
+		createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
+		if err != nil {
+			t.Fatalf("error while creating product %+v", err)
+		}
+		if !reflect.DeepEqual(expectedResult, createdProductOrder) {
+			t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
+		}
+	})
+	t.Run("create product order with amount 1000", func(t *testing.T) {
+
+		expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 2, PackSize: 500})
+
+		productOrderToCreate := &payloads.CreateProductOrderPayload{}
+		productOrderToCreate.Amount = 1000
+		createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
+		if err != nil {
+			t.Fatalf("error while creating product %+v", err)
+		}
+		if !reflect.DeepEqual(expectedResult, createdProductOrder) {
+			t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
+		}
+	})
+	t.Run("create product order with amount 12001", func(t *testing.T) {
+
+		expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 250})
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 1, PackSize: 2000})
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 2, PackSize: 5000})
+
+		productOrderToCreate := &payloads.CreateProductOrderPayload{}
+		productOrderToCreate.Amount = 12001
+		createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
+		if err != nil {
+			t.Fatalf("error while creating product %+v", err)
+		}
+		if len(expectedResult) != len(createdProductOrder) {
+			t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
+		}
+		if !reflect.DeepEqual(expectedResult, createdProductOrder) {
+			t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
+		}
+	})
+}
+func TestWithSmallPackSizes(t *testing.T) {
+	t.Parallel()
+	service := services.GetProductServiceDefaultInstance()
+	ctx := context.Background()
+	createPackSizesPayload := payloads.ChangePackSizesPayload{
+		Sizes: []int{23, 31, 53},
+	}
+	_, err := service.CreatePackSizes(ctx, &createPackSizesPayload)
+	if err != nil {
+		t.Fatalf("failed to insert pack_sizes: %v", err)
+	}
+	t.Run("create product order with amount 263", func(t *testing.T) {
+
+		expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 2, PackSize: 23})
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 7, PackSize: 31})
+
+		productOrderToCreate := &payloads.CreateProductOrderPayload{}
+		productOrderToCreate.Amount = 263
+		createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
+		if err != nil {
+			t.Fatalf("error while creating product %+v", err)
+		}
+		if !reflect.DeepEqual(expectedResult, createdProductOrder) {
+			t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
+		}
+	})
+	t.Run("create product order with amount 500k", func(t *testing.T) {
+
+		expectedResult := make([]*payloads.CreatedPackOrderPayload, 0, 1)
+
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 2, PackSize: 23})
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 7, PackSize: 31})
+		expectedResult = append(expectedResult, &payloads.CreatedPackOrderPayload{AmountOfPacks: 9429, PackSize: 53})
+
+		productOrderToCreate := &payloads.CreateProductOrderPayload{}
+		productOrderToCreate.Amount = 500000
+		createdProductOrder, err := service.CreateProductOrder(ctx, productOrderToCreate)
+		if err != nil {
+			t.Fatalf("error while creating product %+v", err)
+		}
+		if !reflect.DeepEqual(expectedResult, createdProductOrder) {
+			t.Fatalf("create product order failed to produce expected result. \n received: %v", createdProductOrder)
+		}
 	})
 }
